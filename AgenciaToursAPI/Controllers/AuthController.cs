313796a23257c3b6e8e82b03b04e0a82f9f883fb
Anderson-Cruz-V <sd-2022-04079@ufhec.edu.do>
2026-07-27 22:56:48 -0400

    CAMBIOS

--- AgenciaToursAPI/Controllers/AuthController.cs
+++ AgenciaToursAPI/Controllers/AuthController.cs
@@ -1,12 +1,4 @@
-﻿using AgenciaToursAPI.Data;
-using AgenciaToursAPI.Models;
-using AgenciaToursAPI.ModelosTransferencia;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
+﻿
 
 namespace AgenciaToursAPI.Controllers
 {
